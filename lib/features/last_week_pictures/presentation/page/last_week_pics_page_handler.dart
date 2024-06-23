6aa66d6ed879d894fcc525a0849edef3e49e6a16
Matias Raverta <milodude023@gmail.com>
2024-06-23 12:24:22 -0300

    [32] - Modified UI to hold search input box

--- lib/features/last_week_pictures/presentation/page/last_week_pics_page_handler.dart
+++ lib/features/last_week_pictures/presentation/page/last_week_pics_page_handler.dart
@@ -8,6 +8,7 @@ import 'package:nasa_api/features/last_week_pictures/presentation/page/pictures_
 import 'package:nasa_api/generated/l10n.dart';
 
 import '../../../../core/widgets/generic_animated_title.dart';
+import '../../../../core/widgets/generic_input.dart';
 import '../bloc/apod_bloc.dart';
 
 /// LastWeekPicsPageHandler
@@ -27,10 +28,28 @@ class LastWeekPicsPageHandler extends StatefulWidget {
 
 class _LastWeekPicsPageHandlerState extends State<LastWeekPicsPageHandler> {
   bool currentStatus = false;
+  late final TextEditingController _inputController;
+
+  void _changeAction(String input) {
+    if (input.isEmpty) {
+      Modular.get<ApodBloc>().add(
+          SearchPicsEvent(searchInput: input, hasConnection: currentStatus));
+      return;
+    }
+
+    if (input.length > 3) {
+      //Call method from bloc to search
+      DebugProvider.debugLog('Calling search method with input: $input');
+      Modular.get<ApodBloc>().add(
+          SearchPicsEvent(searchInput: input, hasConnection: currentStatus));
+    }
+  }
+
   @override
   void initState() {
     super.initState();
     widget.connectivityProvider.checkInitialConnection();
+    _inputController = TextEditingController();
   }
 
   @override
@@ -53,23 +72,34 @@ class _LastWeekPicsPageHandlerState extends State<LastWeekPicsPageHandler> {
               Modular.get<ApodBloc>()
                   .add(GetLastWeekPicsEvent(hasConnection: snapshot.data!));
             }
-            return BlocBuilder<ApodBloc, ApodState>(
-              builder: (BuildContext context, ApodState state) {
-                if (state is ApodInitial || state is ApodLoading) {
-                  return const GenericCircularProgressindicator();
-                } else if (state is ApodLoaded) {
-                  return PicturesListPage(
-                    pictures: state.picturesList,
-                  );
-                } else if (state is ApodError) {
-                  return Center(child: Text(state.errorMessage));
-                } else if (state is ApodNoConnectionError) {
-                  return Center(
-                      child:
-                          Text(AppLocalizations.of(context).noConnectionError));
-                }
-                return const Center(child: Text('Unhandled state'));
-              },
+            return Column(
+              children: [
+                const SizedBox(height: 10),
+                GenericInputBox(
+                  textInputController: _inputController,
+                  width: MediaQuery.of(context).size.width * 0.9,
+                  title: AppLocalizations.of(context).search,
+                  enabled: true,
+                  leadIcon: null,
+                  onChangedAction: _changeAction,
+                ),
+                BlocBuilder<ApodBloc, ApodState>(
+                  builder: (BuildContext context, ApodState state) {
+                    if (state is ApodInitial || state is ApodLoading) {
+                      return const GenericCircularProgressindicator();
+                    } else if (state is ApodLoaded) {
+                      return PicturesListPage(pictures: state.picturesList);
+                    } else if (state is ApodError) {
+                      return Center(child: Text(state.errorMessage));
+                    } else if (state is ApodNoConnectionError) {
+                      return Center(
+                          child: Text(
+                              AppLocalizations.of(context).noConnectionError));
+                    }
+                    return const Center(child: Text('Unhandled state'));
+                  },
+                ),
+              ],
             );
           }
         },
--- lib/features/last_week_pictures/presentation/page/pictures_list_page.dart
+++ lib/features/last_week_pictures/presentation/page/pictures_list_page.dart
@@ -1,8 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_animate/flutter_animate.dart';
-import 'package:nasa_api/core/providers/debug_provider.dart';
-import 'package:nasa_api/core/widgets/generic_input.dart';
-import 'package:nasa_api/generated/l10n.dart';
 
 import '../../domain/entity/picture_of_the_day.dart';
 import '../widgets/apod_card.dart';
@@ -10,55 +7,24 @@ import '../widgets/apod_card.dart';
 /// PicturesListPage
 ///
 /// Page that renders a list of pictures of the day.
-class PicturesListPage extends StatefulWidget {
-  const PicturesListPage({super.key, required this.pictures});
+class PicturesListPage extends StatelessWidget {
+  const PicturesListPage({
+    super.key,
+    required this.pictures,
+  });
   final List<PictureOfTheDay> pictures;
 
   @override
-  State<PicturesListPage> createState() => _PicturesListPageState();
-}
-
-class _PicturesListPageState extends State<PicturesListPage> {
-  late final TextEditingController _inputController;
-
-  @override
-  void initState() {
-    super.initState();
-    _inputController = TextEditingController();
-  }
-
-  void _changeAction(String input) {
-    if (input.length > 3) {
-      //Call method from bloc to search
-      DebugProvider.debugLog('Calling search method');
-    }
-  }
-
-  @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        const SizedBox(height: 10),
-        GenericInputBox(
-          textInputController: _inputController,
-          width: MediaQuery.of(context).size.width * 0.9,
-          title: AppLocalizations.of(context).search,
-          enabled: true,
-          leadIcon: null,
-          onChangedAction: _changeAction,
-        ),
-        Expanded(
-          child: ListView.builder(
-            itemCount: widget.pictures.length,
-            itemBuilder: (BuildContext context, int index) {
-              return ApodCard(pictureOfTheDay: widget.pictures[index])
-                  .animate(delay: Duration(milliseconds: 100 * index))
-                  .slide(
-                      begin: Offset.fromDirection(0, 1), curve: Curves.easeIn);
-            },
-          ),
-        ),
-      ],
+    return Expanded(
+      child: ListView.builder(
+        itemCount: pictures.length,
+        itemBuilder: (BuildContext context, int index) {
+          return ApodCard(pictureOfTheDay: pictures[index])
+              .animate(delay: Duration(milliseconds: 100 * index))
+              .slide(begin: Offset.fromDirection(0, 1), curve: Curves.easeIn);
+        },
+      ),
     );
   }
 }
