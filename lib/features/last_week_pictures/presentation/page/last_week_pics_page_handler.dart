4ee12c60723d02f68378f93acae2f175769ef596
Matias Raverta <milodude023@gmail.com>
2024-06-23 10:05:15 -0300

    [28] - Modified las week pics page handler to listen to the connection state

--- lib/features/last_week_pictures/presentation/page/last_week_pics_page_handler.dart
+++ lib/features/last_week_pictures/presentation/page/last_week_pics_page_handler.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_modular/flutter_modular.dart';
+import 'package:nasa_api/core/providers/connectivity_provider.dart';
+import 'package:nasa_api/core/providers/debug_provider.dart';
 import 'package:nasa_api/core/widgets/generic_circular_progress_indicator.dart';
 import 'package:nasa_api/features/last_week_pictures/presentation/page/pictures_list_page.dart';
 import 'package:nasa_api/generated/l10n.dart';
@@ -12,7 +14,11 @@ import '../bloc/apod_bloc.dart';
 ///
 /// Handler to call the event from APOD bloc to load all the pics.
 class LastWeekPicsPageHandler extends StatefulWidget {
-  const LastWeekPicsPageHandler({super.key});
+  final ConnectivityProvider connectivityProvider;
+  const LastWeekPicsPageHandler({
+    super.key,
+    required this.connectivityProvider,
+  });
 
   @override
   State<LastWeekPicsPageHandler> createState() =>
@@ -20,11 +26,11 @@ class LastWeekPicsPageHandler extends StatefulWidget {
 }
 
 class _LastWeekPicsPageHandlerState extends State<LastWeekPicsPageHandler> {
+  bool currentStatus = false;
   @override
   void initState() {
     super.initState();
-    //Need to call modular.
-    Modular.get<ApodBloc>().add(GetLastWeekPicsEvent());
+    widget.connectivityProvider.checkInitialConnection();
   }
 
   @override
@@ -35,18 +41,37 @@ class _LastWeekPicsPageHandlerState extends State<LastWeekPicsPageHandler> {
           child: Text(AppLocalizations.of(context).nasaAppTitle),
         ),
       ),
-      body: BlocBuilder<ApodBloc, ApodState>(
-        builder: (BuildContext context, ApodState state) {
-          if (state is ApodInitial || state is ApodLoading) {
+      body: StreamBuilder(
+        stream: widget.connectivityProvider.connectionStatusStream,
+        builder: (context, snapshot) {
+          DebugProvider.debugLog('Has connection: ${snapshot.data}');
+          if (!snapshot.hasData) {
             return const GenericCircularProgressindicator();
-          } else if (state is ApodLoaded) {
-            return PicturesListPage(
-              pictures: state.picturesList,
+          } else {
+            if (currentStatus != snapshot.data!) {
+              currentStatus = snapshot.data!;
+              Modular.get<ApodBloc>()
+                  .add(GetLastWeekPicsEvent(hasConnection: snapshot.data!));
+            }
+            return BlocBuilder<ApodBloc, ApodState>(
+              builder: (BuildContext context, ApodState state) {
+                if (state is ApodInitial || state is ApodLoading) {
+                  return const GenericCircularProgressindicator();
+                } else if (state is ApodLoaded) {
+                  return PicturesListPage(
+                    pictures: state.picturesList,
+                  );
+                } else if (state is ApodError) {
+                  return Center(child: Text(state.errorMessage));
+                } else if (state is ApodNoConnectionError) {
+                  return Center(
+                      child:
+                          Text(AppLocalizations.of(context).noConnectionError));
+                }
+                return const Center(child: Text('Unhandled state'));
+              },
             );
-          } else if (state is ApodError) {
-            return Center(child: Text(state.errorMessage));
           }
-          return const Center(child: Text('Unhandled state'));
         },
       ),
     );
